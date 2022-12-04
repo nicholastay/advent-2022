dbca2bdc8228e1fff50944b70fee997a6c1efccf
Nicholas Tay <nick@windblume.net>
2022-12-04 23:24:11 +1100

    Day04 part02

--- day04/day04.cpp
+++ day04/day04.cpp
@@ -21,7 +21,8 @@ int main(int argc, char** argv)
 
     std::ifstream file(argv[1]);
 
-    int ranges = 0;
+    int part1_ranges = 0;
+    int part2_ranges = 0;
     for (std::string line; std::getline(file, line); ) {
         std::string::size_type dash1_pos = line.find("-");
         std::string::size_type dash2_pos = line.rfind("-");
@@ -34,16 +35,21 @@ int main(int argc, char** argv)
         int e2s1 = parse_int_from_pos(line.data(), comma_pos+1, dash2_pos);
         int e2s2 = parse_int_from_pos(line.data(), dash2_pos+1, line.size());
 
-        if (e2s1 >= e1s1 && e2s2 <= e1s2) {
-            // e1's stuff is all in e2
-            ++ranges;
-        } else if (e1s1 >= e2s1 && e1s2 <= e2s2) {
-            // e2's stuff is all in e1
-            ++ranges;
-        }
+        // PART 1
+        if (
+            (e2s1 >= e1s1 && e2s2 <= e1s2) // e1's stuff is all in e2
+         || (e1s1 >= e2s1 && e1s2 <= e2s2) // e2's stuff is all in e1
+        ) ++part1_ranges;
+
+        // PART 2
+        if (
+            (e1s1 <= e2s1 && e1s2 >= e2s1) // at some point, e2 is in e1
+         || (e2s1 <= e1s1 && e2s2 >= e1s1) // at some point, e1 is in e2
+        ) ++part2_ranges;
     }
 
-    std::cout << "Ranges fully containing the other range: " << ranges << "\n";
+    std::cout << "Part 1 - Ranges fully containing the other range: " << part1_ranges << "\n";
+    std::cout << "Part 2 - Ranges at least partially containing the other range: " << part2_ranges << "\n";
 
     return 0;
 }
