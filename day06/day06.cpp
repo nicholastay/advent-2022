38942e90751bbbe4216a71cd77ccd8d5650149d1
Nicholas Tay <nick@windblume.net>
2022-12-08 09:28:51 +1100

    Day06 part02
    Felt easier than day05 by quite a bit, or did I just overcomplicate
    day05? haha

--- day06/day06.cpp
+++ day06/day06.cpp
@@ -3,6 +3,34 @@
 #include <cassert>
 #include <array>
 
+size_t find_marker(std::string const& line, size_t window_size)
+{
+    assert(line.size() >= window_size);
+
+    // Do sliding window
+    constexpr size_t n = 'z' - 'a' + 1; // Enough to store all letters and access quickly
+    std::array<bool, n> letters;
+
+    for (size_t i = window_size - 1; i < line.size(); ++i) {
+        letters.fill(false);
+
+        bool found = true;
+        for (size_t a = i - window_size + 1; a <= i; ++a) {
+            char c = line[a] - 'a';
+            if (letters[c]) {
+                found = false;
+                break;
+            }
+            letters[c] = true;
+        }
+
+        if (found)
+            return i + 1; // 1-indexed
+    }
+
+    assert(false && "unreachable");
+}
+
 int main(int argc, char** argv)
 {
     if (argc < 2) {
@@ -12,35 +40,13 @@ int main(int argc, char** argv)
 
     std::ifstream file(argv[1]);
 
-
     std::string line;
     std::getline(file, line);
-    assert(line.size() >= 4);
 
-    constexpr size_t n = 'z' - 'a' + 1; // Enough to store all letters and access quickly
-    int processed = -1;
+    size_t part1 = find_marker(line, 4);
+    std::cout << "Part 1 - Characters processed before first marker: " << part1 << "\n";
 
-    // Do sliding window
-    std::array<bool, n> letters;
-    for (size_t i = 3; i < line.size(); ++i) {
-        letters.fill(false);
-
-        bool found = true;
-        for (size_t a = i - 3; a <= i; ++a) {
-            char c = line[a] - 'a';
-            if (letters[c]) {
-                found = false;
-                break;
-            }
-            letters[c] = true;
-        }
-
-        if (found) {
-            processed = i + 1; // 1-indexed
-            break;
-        }
-    }
-
-    std::cout << "Part 1 - Characters processed before first marker: " << processed << "\n";
+    size_t part2 = find_marker(line, 14);
+    std::cout << "Part 2 - Characters processed before first marker: " << part2 << "\n";
     return 0;
 }
